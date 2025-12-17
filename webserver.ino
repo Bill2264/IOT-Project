05ffca6c891bc7159429d4fe7fcab652fad8b20e
Billy Lis-Murphy - STUDENT <g00465240@atu.ie>
2025-12-17 11:28:22 +0000

    Final code for christmas demo

---
+++ webserver.ino
@@ -0,0 +1,21 @@
+
+
+void handleRoot() {
+  String message = homePagePart1 + getTemp() + homePagePart2 + getHumid() + homePagePart3;
+  server.send(200, "text/html", message);
+}
+
+void handleNotFound() {
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET) ? "GET" : "POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i = 0; i < server.args(); i++) {
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+}
